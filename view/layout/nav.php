b4273144903ff64f9c33ec622418e48dfb0b4a87
danghungquan <quandang097@gmail.com>
2022-01-25 08:37:15 +0700

    first commit

--- view/layout/nav.php
+++ view/layout/nav.php
@@ -5,36 +5,7 @@
         </b>
         <hr style="margin: 0px;">
     </div>
-    <div id="list_search">
-        <?php
-
-        while ($row = mysqli_fetch_assoc($ports_select_selectNewest)) { ?>
-            <table>
-                <tr>
-                    <td class="block-gd-td">
-                        <div class="block-gd">
-                            <div class="block-img">
-                                <img src="<?php echo $row['image']; ?>" alt="">
-                            </div>
-                        </div>
-                    </td>
-                    <td>
-                        <div class="block-block">
-                            <a href="">
-                                <?php echo $row['title']; ?>
-                            </a>
-                        </div>
-                        <div class="block-day">
-                            <?php echo date("d M, Y", strtotime($row['create_at'])); ?>
-                        </div>
-                    </td>
-                </tr>
-            </table>
-            <div class="nav-hr">
-                <hr>
-            </div>
-        <?php } ?>
-    </div>
+    ádasdsdasd
     <div class="nav-form">
         <form method="GET" action="">
             <div class="nav-form-search">
