8837544ae7faa381dfeb10dc35ab2b43a24f6d62
danghungquan <quandang097@gmail.com>
2022-01-25 14:08:10 +0700

    first commit

--- backEnd/connectionDB.php
+++ backEnd/connectionDB.php
@@ -1,13 +1,12 @@
 <?php
-$servername = "185.27.134.10";
+$host = "sql213.epizy.com";
 $username = "epiz_30897982";
 $password = "KbN8GQZQsjHI";
 $dbname = "epiz_30897982_hueids";
 
 // Create connection
-$conn =  mysqli_connect($servername, $username, $password, $dbname);
-$conn->set_charset("utf8");
-
+$conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+$conn = connect();
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
